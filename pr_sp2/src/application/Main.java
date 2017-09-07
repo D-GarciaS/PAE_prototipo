2f29584a594023116a0a078ec929b69bedfd240e
DESKTOP-6P02NRP\Damian Garcia <damian95.gs@gmail.com>
2017-09-07 01:24:57 -0500

    layout: add scroll and vbox panes

--- pr_sp2/src/application/Main.java
+++ pr_sp2/src/application/Main.java
@@ -1,16 +1,29 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			ScrollPane root = new ScrollPane();
+			VBox lista =  new VBox();
+			
+			Rectangle n0 = new Rectangle(100,30);
+			Rectangle n1 = new Rectangle(100,30);
+			Rectangle n2 = new Rectangle(100,30);
+			Rectangle n3 = new Rectangle(100,30);
+
+			lista.getChildren().addAll(n0,n1,n2,n3);
+			
+			root.setContent(lista);
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
