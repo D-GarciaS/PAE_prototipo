f3633082a502570e10cb08d00a1b49348b58f1ca
ppgmz <ie697875@iteso.mx>
2017-09-07 01:17:44 -0500

    incoming file first template

--- pr_sp2/src/application/Main.java
+++ pr_sp2/src/application/Main.java
@@ -1,17 +1,24 @@
 package application;
 	
+
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			URL resource = getClass().getResource("incoming_file_layout.fxml");
+			HBox root = FXMLLoader.load(resource);
+			
+			Scene scene = new Scene(root,430,80);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
