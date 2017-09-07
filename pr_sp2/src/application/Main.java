7e904e4ff4faafa9eff9962b2a95b33e5a06d007
ppgmz <ie697875@iteso.mx>
2017-09-07 02:45:38 -0500

    button in main

--- pr_sp2/src/application/Main.java
+++ pr_sp2/src/application/Main.java
@@ -1,15 +1,27 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	Button transmit;
+	Button receive;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		
+		transmit = new Button("Simulate transmision");
+		
 		try {
 
 			Stage newStage = new Stage();
@@ -21,6 +33,25 @@ public class Main extends Application {
 			newStage.show();
 
 			VBox root = new VBox();
+			
+			EventHandler handler = new EventHandler(){
+
+				@Override
+				public void handle(Event event) {				
+					if(event.getEventType() == MouseEvent.MOUSE_CLICKED){
+					
+						tl.add("Archivo de audio", "audio_icon_template.png");
+						
+					}
+
+				}
+
+			};
+			
+			transmit.addEventHandler(MouseEvent.MOUSE_CLICKED, handler);
+			
+			root.getChildren().add(transmit);
+			
 			Scene scene = new Scene(root, 200, 200);
 
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
--- pr_sp2/src/application/TransmissionList.java
+++ pr_sp2/src/application/TransmissionList.java
@@ -3,11 +3,15 @@ package application;
 import java.io.IOException;
 import java.net.URL;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TransmissionList {
@@ -19,6 +23,7 @@ public class TransmissionList {
 	}
 	
 	void add(){
+		Text text;
 		URL resource = getClass().getResource("incoming_file_layout.fxml");
 		HBox h = null;
 		try {
@@ -27,7 +32,44 @@ public class TransmissionList {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+				
 		lista.getChildren().addAll(h);
+		
+	}
+	
+	void add(String fileName){
+		
+		URL resource = getClass().getResource("incoming_file_layout.fxml");
+		HBox h = null;
+		try {
+			h = FXMLLoader.load(resource);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		((Text)h.lookup("#fileName")).setText(fileName);		
+		
+		lista.getChildren().addAll(h);
+	}
+	
+	
+	void add(String fileName, String imageName){
+		
+		URL resource = getClass().getResource("incoming_file_layout.fxml");
+		HBox h = null;
+		try {
+			h = FXMLLoader.load(resource);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		((Text)h.lookup("#fileName")).setText(fileName);	
+		((ImageView)h.lookup("#fileIcon")).setImage(new Image(getClass().getResource("icon.png").toExternalForm()));
+		
+		lista.getChildren().addAll(h);
+		
 	}
 	
 	void create(Stage primaryStage){
