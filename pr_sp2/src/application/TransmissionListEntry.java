d3b926c03e4bb3c28af5ddcac2fd507b28b78b55
ppgmz <ie697875@iteso.mx>
2017-09-07 09:38:55 -0500

    styles

--- pr_sp2/src/application/TransmissionListEntry.java
+++ pr_sp2/src/application/TransmissionListEntry.java
@@ -1,8 +1,15 @@
 package application;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 
+import javax.swing.ImageIcon;
+import javax.swing.filechooser.FileSystemView;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
@@ -24,17 +31,26 @@ public class TransmissionListEntry {
 	public TransmissionListEntry(String fileName, String imageName) {
 		URL resource = getClass().getResource("incoming_file_layout.fxml");
 		try {
+			
 			h = FXMLLoader.load(resource);
+			h.getStyleClass().add("hbox");
 			v = (VBox) h.getChildren().get(1);
 			pb = (ProgressBar) v.getChildren().get(1);
+			
 			cancel = (Button) h.getChildren().get(3);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		((Text)h.lookup("#fileName")).setText(fileName);	
-		((ImageView)h.lookup("#fileIcon")).setImage(new Image(getClass().getResource("icon.png").toExternalForm()));
+		
+		 Image fxImage = getFileIcon("icon.png");
+         ImageView imageView = new ImageView(fxImage);
+		
+		
+		((ImageView)h.lookup("#fileIcon")).setImage(getFileIcon("icon.png"));
 		
 		cancel.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {				
 			if(event.getEventType() == MouseEvent.MOUSE_CLICKED){
@@ -52,4 +68,75 @@ public class TransmissionListEntry {
 	void setProgress(double d){
 		pb.setProgress(d);
 	}
+	
+	
+	private static javax.swing.Icon getJSwingIconFromFileSystem(File file) {
+
+        // Windows {
+        FileSystemView view = FileSystemView.getFileSystemView();
+        javax.swing.Icon icon = view.getSystemIcon(file);
+        // }
+
+        // OS X {
+        //final javax.swing.JFileChooser fc = new javax.swing.JFileChooser();
+        //javax.swing.Icon icon = fc.getUI().getFileView(fc).getIcon(file);
+        // }
+
+        return icon;
+    }
+	
+    static HashMap<String, Image> mapOfFileExtToSmallIcon = new HashMap<String, Image>();
+	
+    private static String getFileExt(String fname) {
+        String ext = ".";
+        int p = fname.lastIndexOf('.');
+        if (p >= 0) {
+            ext = fname.substring(p);
+        }
+        return ext.toLowerCase();
+    }
+	
+    private static Image getFileIcon(String fname) {
+        final String ext = getFileExt(fname);
+
+        Image fileIcon = mapOfFileExtToSmallIcon.get(ext);
+        if (fileIcon == null) {
+
+            javax.swing.Icon jswingIcon = null; 
+
+            File file = new File(fname);
+            if (file.exists()) {
+                jswingIcon = getJSwingIconFromFileSystem(file);
+            }
+            else {
+                File tempFile = null;
+                try {
+                    tempFile = File.createTempFile("icon", ext);
+                    jswingIcon = getJSwingIconFromFileSystem(tempFile);
+                }
+                catch (IOException ignored) {
+                    // Cannot create temporary file. 
+                }
+                finally {
+                    if (tempFile != null) tempFile.delete();
+                }
+            }
+
+            if (jswingIcon != null) {
+                fileIcon = jswingIconToImage(jswingIcon);
+                mapOfFileExtToSmallIcon.put(ext, fileIcon);
+            }
+        }
+
+        return fileIcon;
+    }
+
+    private static Image jswingIconToImage(javax.swing.Icon jswingIcon) {
+        BufferedImage bufferedImage = new BufferedImage(jswingIcon.getIconWidth(), jswingIcon.getIconHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        jswingIcon.paintIcon(null, bufferedImage.getGraphics(), 0, 0);
+        return SwingFXUtils.toFXImage(bufferedImage, null);
+    }
+
+	
 }
